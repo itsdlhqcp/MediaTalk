994912323efeab30c7cae543e3a0b167c4783d24
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-09-21 13:04:18 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+     sm: "340px",
+     md: "540px",
+     lg: "768px",
+     xl: "1180px"
+    },
+    extend: {},
+    fontFamily: {
+
+    },
+    container: {
+      center: true, 
+      padding: {
+        DEFAULT: '12px',
+        md: "32px"
+      }
+    }
+  },
+  plugins: [],
+}
